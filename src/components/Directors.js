5c6b8da816244e78eb906e32da2a1b6be2327045
NicMortelliti <npmortelliti@gmail.com>
2022-04-06 14:05:28 -0700

    Add data to Directors component.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorArray = directors.map((director) => {
+    return (
+      <div>
+        <h1>{director.name}</h1>
+        <ul>
+          {director.movies.map((movie) => <li>{movie}</li> )}
+        </ul>
+      </div>
+    )
+  })
+  
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorArray}
+    </div>
+  );
 }
 
 export default Directors;
