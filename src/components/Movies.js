9d21cbcf8b650f189ff77444307a774559d4f9fc
NicMortelliti <npmortelliti@gmail.com>
2022-04-06 14:01:42 -0700

    Add movie data to Movies component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieArray = movies.map((movie) => {
+    return (
+      <div>
+        <h1>{movie.title}</h1>
+        <h3>{movie.time}</h3>
+        <ul>
+          {movie.genres.map((genre) => <li>{genre}</li> )}
+        </ul>
+      </div>
+    )
+  })
+  
+  return (
+    <div>
+      <h1>Moves Page</h1>
+      {movieArray}
+    </div>
+  );
 }
 
 export default Movies;
