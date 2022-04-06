caf8c34db67c98ea2d2c6cd81454887ea13f55fd
NicMortelliti <npmortelliti@gmail.com>
2022-04-06 14:09:07 -0700

    Add data to Actors component

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorArray = actors.map((actor) => {
+    return (
+      <div>
+        <h1>{actor.name}</h1>
+        <ul>
+          {actor.movies.map((movie) => <li>{movie}</li> )}
+        </ul>
+      </div>
+    )
+  })
+  
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorArray}
+    </div>
+  );
 }
 
 export default Actors;
