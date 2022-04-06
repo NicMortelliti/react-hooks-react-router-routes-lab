df774898e6bfcbea4329585cf003f2e19d8e3193
NicMortelliti <npmortelliti@gmail.com>
2022-04-06 13:48:03 -0700

    Adding routing in App. Add placeholder in Navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div><h1>NavBar placeholder</h1></div>;
 }
 
 export default NavBar;
